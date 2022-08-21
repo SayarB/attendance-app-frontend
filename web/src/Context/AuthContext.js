c91dfdab98d72922bdc2ad54f0fb1dfe2551de62
SayarB <sayarbhattacharyya03@gmail.com>
2022-08-21 22:10:36 +0530

    fix: linting fix

--- web/src/Context/AuthContext.js
+++ web/src/Context/AuthContext.js
@@ -1,46 +1,46 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { createContext, useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 import {
   onAuthStateChanged,
   signInWithPhoneNumber,
-  signOut,
-} from "firebase/auth";
-const AuthContext = createContext();
+  signOut
+} from 'firebase/auth'
+const AuthContext = createContext()
 
-export function useAuth() {
-  return useContext(AuthContext);
+export function useAuth () {
+  return useContext(AuthContext)
 }
 
-export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+export function AuthProvider ({ children }) {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [loading, setLoading] = useState(true)
   const value = {
     currentUser,
     signIn,
     signOut: logOut,
-    loading,
-  };
-
-  function signIn(recaptchaVerifier, phno) {
-    return signInWithPhoneNumber(auth, phno, recaptchaVerifier);
+    loading
   }
-  function logOut() {
-    return signOut(auth);
+
+  function signIn (recaptchaVerifier, phno) {
+    return signInWithPhoneNumber(auth, phno, recaptchaVerifier)
+  }
+  function logOut () {
+    return signOut(auth)
   }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log("state changed");
-      setCurrentUser(user);
-      setLoading(false);
-    });
-    return unsubscribe;
-  }, []);
+      console.log('state changed')
+      setCurrentUser(user)
+      setLoading(false)
+    })
+    return unsubscribe
+  }, [])
 
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
-  );
+  )
 }
