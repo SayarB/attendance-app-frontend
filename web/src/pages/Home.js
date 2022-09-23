7cb559e478b575c48e15bf7910a6bc8cc50cbaf2
SayarB <sayarbhattacharyya03@gmail.com>
2022-09-23 11:50:23 +0530

    fix: changed club name to testing in attendance state request

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -42,7 +42,7 @@ function Home() {
     else retrieveIdToken();
 
     axios
-      .get("https://attendencegdsc.herokuapp.com/attendence_state/tests")
+      .get("https://attendencegdsc.herokuapp.com/attendence_state/testing")
       .then((res) => {
         console.log(res.data);
         setAttendanceOpen(res.data.state);
