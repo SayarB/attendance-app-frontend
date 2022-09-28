54869ac2094a7cf64d28f954af9968af790c9c4b
SayarB <sayarbhattacharyya03@gmail.com>
2022-09-28 15:02:04 +0530

    fix: toast added to notify unopened attendance

--- web/src/App.js
+++ web/src/App.js
@@ -1,38 +1,38 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Welcome from './pages/Welcome'
-import Login from './pages/Login'
-import { AuthProvider } from './Context/AuthContext'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import FailurePage from './pages/FailurePage'
-import SuccessPage from './pages/SuccessPage'
-function App () {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import { AuthProvider } from "./Context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import FailurePage from "./pages/FailurePage";
+import SuccessPage from "./pages/SuccessPage";
+function App() {
   return (
     <Router>
       <AuthProvider>
         <ToastContainer
-          position='top-right'
-          theme='colored'
-          autoClose={5000}
+          position="top-right"
+          theme="colored"
+          autoClose={4000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
           rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
         />
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/failure' element={<FailurePage />} />
-          <Route exact path='/success' element={<SuccessPage />} />
-          <Route exact path='welcome' element={<Welcome />} />
-          <Route exact path='login' element={<Login />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/failure" element={<FailurePage />} />
+          <Route exact path="/success" element={<SuccessPage />} />
+          <Route exact path="welcome" element={<Welcome />} />
+          <Route exact path="login" element={<Login />} />
         </Routes>
       </AuthProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,110 +1,110 @@
 // import { signOut } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Button from '../Components/Button'
-import { useAuth } from '../Context/AuthContext'
-import axios from 'axios'
-import { getIdToken } from 'firebase/auth'
-import { toast } from 'react-toastify'
-import Lottie from 'react-lottie'
-import animationData from '../assets/loading'
-const dateString = new Date().toLocaleString('en-GB', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric'
-})
-function Home () {
-  const authState = useAuth()
-  const navigate = useNavigate()
-  const [token, setToken] = useState('')
-  const [attendanceOpen, setAttendanceOpen] = useState()
-  const [isLoading, setLoading] = useState(false)
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../Components/Button";
+import { useAuth } from "../Context/AuthContext";
+import axios from "axios";
+import { getIdToken } from "firebase/auth";
+import { toast } from "react-toastify";
+import Lottie from "react-lottie";
+import animationData from "../assets/loading";
+const dateString = new Date().toLocaleString("en-GB", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+function Home() {
+  const authState = useAuth();
+  const navigate = useNavigate();
+  const [token, setToken] = useState("");
+  const [attendanceOpen, setAttendanceOpen] = useState();
+  const [isLoading, setLoading] = useState(false);
   const [position, setPosition] = useState({
-    lat: '',
-    long: ''
-  })
+    lat: "",
+    long: "",
+  });
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
 
     navigator.geolocation.getCurrentPosition((pos) => {
       console.log(
-        'lat = ' + pos.coords.latitude + '\nlong = ' + pos.coords.longitude
-      )
-      setPosition({ lat: pos.coords.latitude, long: pos.coords.longitude })
-    })
+        "lat = " + pos.coords.latitude + "\nlong = " + pos.coords.longitude
+      );
+      setPosition({ lat: pos.coords.latitude, long: pos.coords.longitude });
+    });
     const retrieveIdToken = async () => {
-      const idToken = await getIdToken(authState.currentUser, true)
-      console.log(idToken)
-      setToken(idToken)
-    }
+      const idToken = await getIdToken(authState.currentUser, true);
+      console.log(idToken);
+      setToken(idToken);
+    };
 
-    if (authState.currentUser === null) navigate('/login')
-    else retrieveIdToken()
+    if (authState.currentUser === null) navigate("/welcome");
+    else retrieveIdToken();
 
     axios
-      .get('https://attendencegdsc.herokuapp.com/attendence_state/testing')
+      .get("https://attendencegdsc.herokuapp.com/attendence_state/testing")
       .then((res) => {
-        console.log(res.data)
-        setAttendanceOpen(res.data.state)
-        setLoading(false)
+        console.log(res.data);
+        setAttendanceOpen(res.data.state);
+        setLoading(false);
       })
       .catch((err) => {
-        setLoading(false)
-        toast.error(err.message)
-      })
-  }, [])
+        setLoading(false);
+        toast.error(err.message);
+      });
+  }, []);
 
   const handleSignOut = async () => {
     try {
-      await authState.signOut()
-      navigate('/login')
+      await authState.signOut();
+      navigate("/login");
     } catch (err) {
-      console.log(err.message)
+      console.log(err.message);
     }
-    console.log('logged out')
-  }
+    console.log("logged out");
+  };
 
   const handleClick = async () => {
-    console.log('Open attendence')
+    console.log("Open attendence");
     // console.log(new Date().toUTCString());
-    setLoading(true)
+    setLoading(true);
     try {
       const res = await axios.post(
-        'https://attendencegdsc.herokuapp.com/give_attendence/',
+        "https://attendencegdsc.herokuapp.com/give_attendence/",
         {
-          club_name: 'testing',
+          club_name: "testing",
           token,
           lat: position.lat,
           long: position.long,
-          utc: new Date().toUTCString()
+          utc: new Date().toUTCString(),
         }
-      )
+      );
 
-      setLoading(false)
-      console.log('res = ', res.data)
-      if (res.data.error && res.data.error === 'user not in range') {
-        navigate('/failure')
-      } else navigate('/success')
+      setLoading(false);
+      console.log("res = ", res.data);
+      if (res.data.error && res.data.error === "user not in range") {
+        navigate("/failure");
+      } else navigate("/success");
     } catch (err) {
-      setLoading(false)
-      toast.error('There was some Problem')
+      setLoading(false);
+      toast.error("There was some Problem");
     }
-  }
+  };
 
   const lottieOptions = {
     loop: true,
     autoplay: true,
     animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  }
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <>
       {isLoading && (
-        <div className='fixed w-[100vw] h-[100vh] bg-white z-10'>
-          <div className=' absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <div className="fixed w-[100vw] h-[100vh] bg-white z-10">
+          <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
             <Lottie
               isClickToPauseDisabled
               options={lottieOptions}
@@ -114,30 +114,37 @@ function Home () {
           </div>
         </div>
       )}
-      <div className='flex flex-col h-[100vh] items-center'>
-        <div className=' w-full max-w-2xl p-10 flex items-center justify-between font-sans'>
+      <div className="flex flex-col h-[100vh] items-center">
+        <div className=" w-full max-w-2xl p-10 flex items-center justify-between font-sans">
           <div>
-            <p className='text-xl'>Welcome 👋</p>
-            <p className='text-3xl'>{authState.currentUser?.displayName}</p>
+            <p className="text-xl">Welcome 👋</p>
+            <p className="text-3xl">{authState.currentUser?.displayName}</p>
           </div>
-          <div className='flex flex-col'>
-            <div className=' overflow-hidden rounded-xl w-[4rem] bg-[#F3F3F3]'>
-              <img src='avatar.svg' alt='' className='w-full' />
+          <div className="flex flex-col">
+            <div className=" overflow-hidden rounded-xl w-[4rem] bg-[#F3F3F3]">
+              <img src="avatar.svg" alt="" className="w-full" />
             </div>
             <button onClick={handleSignOut}>Log out</button>
           </div>
         </div>
-        <div className=' w-full border-y-2 py-3 text-center'>
-          <p className=' font-sans text-primary text-xl'>{dateString}</p>
+        <div className=" w-full border-y-2 py-3 text-center">
+          <p className=" font-sans text-primary text-xl">{dateString}</p>
         </div>
-        <div className='flex-1 flex items-center justify-center'>
-          <Button onClick={handleClick} disabled={!attendanceOpen}>
+        <div className="flex-1 flex items-center justify-center">
+          <Button
+            onClick={() => {
+              if (attendanceOpen) handleClick();
+              else toast.error("Attendance not opened yet");
+            }}
+            disabled={!attendanceOpen}
+            className="w-[250px]"
+          >
             Check In
           </Button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
